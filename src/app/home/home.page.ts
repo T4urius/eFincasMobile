0ffe7159c1e8ad5cf21609e9d532259abb811765
Matheus Andrade Uliana <matheus.andrade@sawluz.com.br>
2019-12-04 15:03:22 -0300

    Inserindo estilo em informacoes e adicionando mock de historico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  slideOpts = {
+    speed: 400,
+    initialSlide: 0,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    }
+  }
+
+  items = [
+
+    { id: 1, name: 'Superman' },
+    { id: 2, name: 'Batman' },
+    { id: 3, name: 'BatGirl' },
+    { id: 4, name: 'Robin' },
+    { id: 5, name: 'Flash' },
+    { id: 6, name: 'Superman' },
+    { id: 7, name: 'Batman' },
+    { id: 8, name: 'BatGirl' },
+    { id: 9, name: 'Robin' },
+    { id: 10, name: 'Flash' },
+  ];
+
+  constructor() { }
 
 }
