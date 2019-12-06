58ae96e50f578eaf09f7e285ed3f42ab743cdfc0
Matheus Andrade Uliana <matheus.andrade@sawluz.com.br>
2019-12-06 11:00:48 -0300

    Adicionando classes css e comportamento do mesmo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -9,6 +9,13 @@ import { NavController } from '@ionic/angular';
 export class HomePage {
 
   // @ViewChild('ionSlidesSaldoPagar', { read: ElementRef, static: false }) ionSlide: ElementRef;
+  @ViewChild('eyeOff', { read: ElementRef, static: false }) eyeOff: ElementRef;
+  @ViewChild('totalSaldo', { read: ElementRef, static: false }) totalSaldo: ElementRef;
+  @ViewChild('receber', { read: ElementRef, static: false }) receber: ElementRef;
+
+  @ViewChild('eyePagar', { read: ElementRef, static: false }) eyePagar: ElementRef;
+  @ViewChild('totalPagar', { read: ElementRef, static: false }) totalPagar: ElementRef;
+  @ViewChild('pagar', { read: ElementRef, static: false }) pagar: ElementRef;
 
   slideOpts = {
     speed: 400,
@@ -55,6 +62,30 @@ export class HomePage {
 
   openHome() {
     this.navCtrl.navigateForward('');
+  }
+
+  hideSaldo() {
+    this.eyeOff.nativeElement.style.visibility = 'visible';
+    this.receber.nativeElement.style.visibility = 'hidden';
+    this.totalSaldo.nativeElement.style.visibility = 'hidden';
+  }
+
+  hidePagar() {
+    this.eyePagar.nativeElement.style.visibility = 'visible';
+    this.pagar.nativeElement.style.visibility = 'hidden';
+    this.totalPagar.nativeElement.style.visibility = 'hidden';
+  }
+
+  showSaldo() {
+    this.receber.nativeElement.style.visibility = 'visible';
+    this.totalSaldo.nativeElement.style.visibility = 'visible';
+    this.eyeOff.nativeElement.style.visibility = 'hidden';
+  }
+
+  showPagar() {
+    this.eyePagar.nativeElement.style.visibility = 'hidden';
+    this.pagar.nativeElement.style.visibility = 'visible';
+    this.totalPagar.nativeElement.style.visibility = 'visible';
   }
 
   // openFirst() {
